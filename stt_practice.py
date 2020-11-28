299169738a820cb73dd741c37eda71e95fa1c7ca
KoYejune <koyejune0302@gmail.com>
2020-11-28 17:39:46 +0900

    SpeachToText Practice

---
+++ stt_practice.py
@@ -0,0 +1,23 @@
+# pip install SpeechRecognition
+# pip install pyaudio
+
+import speech_recognition as sr
+
+AUDIO_FILE = "hello.wav"
+
+# audio file을 audio source로 사용합니다
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # 전체 audio file 읽기
+
+# 구글 웹 음성 API로 인식하기 (하루에 제한 50회)
+try:
+    print("Google Speech Recognition thinks you said : " + r.recognize_google(audio, language='ko'))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+# write audio to a WAV file
+with open("microphone-results.wav", "wb") as f:
+    f.write(audio.get_wav_data())
